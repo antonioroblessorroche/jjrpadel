7d771a74845183904e99b18e2718dd32a1f36afc
arobles <antonio.roblessorroche@alum.uca.es>
2025-12-01 16:52:52 +0100

    chore: @AnonymousAllowed

--- src/main/java/com/jjrpadel/app/config/LoginView.java
+++ src/main/java/com/jjrpadel/app/config/LoginView.java
@@ -3,9 +3,11 @@ package com.jjrpadel.app.config;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @Route("login")
 @PageTitle("Login")
+@AnonymousAllowed
 public class LoginView extends LoginOverlay {
 
     public LoginView() {
